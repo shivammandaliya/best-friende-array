1beda83024f275a6f92e85021c40cc6727f956c3
shivammandaliya <101386615+shivammandaliya@users.noreply.github.com>
2022-04-04 22:01:49 +0530

    Add files via upload

---
+++ Best Friends Array/sketch.js
@@ -0,0 +1,21 @@
+var friends =["riya", "jyoti" , "raj" , "ritu" , "avi"];
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  console.log("My best friends are :");
+  for (i=0; i<friends.length; i++) {
+    console.log(friends[i]);
+  }
+
+  //2. print the names in friends array in the console:
+  
+
+}
+
+function draw() {
+    
+}
+
+    
+
